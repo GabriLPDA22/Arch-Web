cbc0552327ba756226c3ec1a08515130338445e9
GabriLPDA22 <gsaiz.bajo@gmail.com>
2026-01-07 13:33:19 +0100

    Add Debug Logging for User Save Operation in UserForm.vue
    - Introduced console logs to display the user payload and oxfordSubtype value during the save operation.
    - Aimed at improving debugging and tracking of user data submissions in the UserForm component.

--- arch-web/src/components/forms/UserForm.vue
+++ arch-web/src/components/forms/UserForm.vue
@@ -728,6 +728,9 @@ const saveUser = async () => {
       delete payload.password
     }
 
+    console.log('📤 Saving user with payload:', JSON.stringify(payload, null, 2))
+    console.log('📤 oxfordSubtype value:', oxfordSubtype.value)
+
     if (isEditing.value) {
       await UserApi.update(props.userId!, payload)
       emit('user-updated')
